57654ccfe2d76c91a0a8eaf2aa9323841620e23f
reficul0 <cheshirwr@mail.ru>
2023-05-13 16:11:07 +0200

    Refactoring

---
+++ untitled1/buisness_logic.hpp
@@ -0,0 +1,106 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <ctype.h>
+#include <cinttypes>
+#include <vector>
+
+#include "constants.hpp"
+
+struct PersonInfo
+{
+    std::string name;
+    int age;
+};
+
+std::ostream& operator<<(std::ostream& os, PersonInfo const &user_data) { 
+    return os << "{ Name: " << user_data.name 
+              << ", Age: " << user_data.age << "}";
+}
+
+void clear_stream(std::istream& stream) {
+    if (std::cin) {
+        std::cin.clear();
+        std::cin.ignore(1000, '\n');
+    }
+}
+
+PersonInfo congratulate() {
+    std::string name;
+    do
+    {
+      std::cout << "Как тебя зовут?" << std::endl;
+      std::cin >> name;
+      clear_stream(std::cin);
+
+      for (char c : name)
+      {
+          if (!isalpha(c))
+          {
+              std::cout << "Неправильный формат имени: можно ввести только буквы,  недопустимый символ: " 
+                        << c << std::endl;
+              continue;
+          }
+      }
+    } while (std::cin);
+
+    int age = 0;
+    do
+    {
+        std::cout << "Сколько тебе лет?" << std::endl;
+        std::cin >> age;
+        clear_stream(std::cin);
+    } while (std::cin);
+
+    std::cout << std::endl
+            << name << ", поздравляю тебя!" << std::endl
+            << "Сегодня тебе " << age << " лет" << std::endl
+            << "Желаю счастья и здоровья!" << std::endl;
+
+    if (age >= 35 && age <= 60)
+    {
+        std::cout << note << std::endl 
+                        << std::endl 
+                        << std::endl;
+    }
+
+    PersonInfo info;
+    info.age = age;
+    info.name = name;
+    return info;
+}
+
+bool run(std::vector<PersonInfo>& data) {
+    bool is_running = true;
+    
+    std::uint16_t menu_item_id = 0;
+    std::cout << welcome_menu << std::endl;
+    std::cin >> menu_item_id;
+
+    switch (menu_item_id)
+    {
+    case 1:
+      {
+        PersonInfo info = congratulate();
+        data.push_back(info);
+      }
+      break;
+    case 2:
+        std::cout << "Цикл завершен\n\n" << std::endl;
+        is_running = false;
+        break;
+    case 3:
+        for(PersonInfo& user_data : data) {
+            std::cout << user_data << ",\n";
+        }
+        break;
+    default:
+        std::cerr << "ERROR: invalid menue item id " << menu_item_id;
+        break;
+    }
+
+    clear_stream(std::cin);
+
+    return is_running;
+}
--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -1,119 +1,42 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <ctype.h>
+﻿#include <locale>
+
+#include "buisness_logic.hpp"
 
 int main()
 {
-    bool is_running = true;
-    while (is_running)
+#if defined(WIN32)
+    setlocale(LC_ALL, "ru_RUS.utf8");
+#endif
+
+    std::vector<PersonInfo> data;
+    while (run(data)) {};
+
+    return EXIT_SUCCESS;
+
     {
+        // int menu2;
+        // std::cout << "Хотите ли вы вывести информацию в таблицу?"
+        //<< std::endl
+        //<< "1. Да" << std::endl
+        //<< "2. Нет" << std::endl;
+        // std::cin >> menu2;
 
-        int menu;
-        std::cout << "Выбери пункт меню, указав соответствующую цифру" << std::endl
-                  << "1. Получить поздравление" << std::endl
-                  << "2. Завершить и выйти из цикла" << std::endl;
-        std::cin >> menu;
-
-        if (menu == 1)
-        {
-            std::string name;
-            std::cout << "Как тебя зовут?" << std::endl;
-            std::cin >> name;
-                bool valid = true;
-                for (char c : name)
-                {
-                    if (!isalpha(c))
-                    {
-                        valid = false;
-                        break;
-                    }
-                }
-
-                if (valid)
-                {
-                    std::cout << "Неправильный формат имени: можно ввести только буквы" << std::endl;
-        }
-
-        int age;
-        std::cout << "Сколько тебе лет?" << std::endl;
-        std::cin >> age;
-
-            if (std::cin.fail()) {
-                std::cout << "Ошибка! Ввести можно только число" << std::endl << std::endl;
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-
-                do {
-                    std::cout << "Сколько тебе лет?" << std::endl;
-                    std::cin >> age;
-
-                    std::cout << std::endl << name << ", поздравляю тебя!" << std::endl;
-                    std::cout << "Сегодня тебе " << age << " лет" << std::endl;
-                    std::cout << "Желаю счастья и здоровья!" << std::endl;
-
-                    if (age >= 35 && age <= 60) {
-                        std::cout << std::endl <<
-                                  "Кризис среднего возраста — это эмоциональное состояние, которое человек испытывает в возрасте 30–50 лет."
-                                  << std::endl <<
-                                  "Чаще всего оно связано с переоценкой жизненного опыта." << std::endl <<
-                                  "Кажется, что многие возможности и мечты юности упущены, при этом старость (или смерть) наступит довольно скоро."
-                                  << std::endl <<
-                                  "У разных людей кризис наступает в разном возрасте, это зависит от индивидуальных особенностей."
-                                  << std::endl <<
-                                  std::endl <<
-                                  "Как пережить кризис среднего возраста:" << std::endl <<
-                                  "1. Заботьтесь о себе" << std::endl <<
-                                  "2. Следите за здоровьем" << std::endl <<
-                                  "3. Примите изменения" << std::endl <<
-                                  "4. Тренируйте мозг" << std::endl <<
-                                  "5. Получайте новые эмоции" << std::endl;
-
-                        std::cout << std::endl << std::endl;
-                        continue;
-                    }
-                    continue;
-                } while (!std::cin.fail());
-            }
-        }
-
-        if (menu == 2) {
-            std::cout << "Цикл завершен" << std::endl << std::endl << std::endl;
-        }
-
-        if (std::cin.fail())
-        {
-            std::cout << "Ошибка! Ввести можно только число 1 или 2" << std::endl << std::endl << std::endl;
-            std::cin.clear();
-            std::cin.ignore(1000, '\n');
-            continue;
-        }
-
-        //int menu2;
-        //std::cout << "Хотите ли вы вывести информацию в таблицу?"
-                  //<< std::endl
-                  //<< "1. Да" << std::endl
-                  //<< "2. Нет" << std::endl;
-        //std::cin >> menu2;
-
-       //if (std::cin.fail()) {
-            //std::cout << "Ошибка! Ввести можно только число 1 или 2" << std::endl << std::endl << std::endl;
-            //std::cin.clear();
-            //std::cin.ignore(1000, '\n');
+        // if (std::cin.fail()) {
+        // std::cout << "Ошибка! Ввести можно только число 1 или 2" << std::endl << std::endl << std::endl;
+        // std::cin.clear();
+        // std::cin.ignore(1000, '\n');
         //} else if (menu2 == 1) {
-            //std::const int ROWS = n;
-            //std::const int COLS = 2;
+        // const int ROWS = n;
+        // std::const int COLS = 2;
         //}
 
-            //std::ofstream file(NameAges.txt);
-            //if (!file.is_open()) {
-                //std::cout << "Закройте файл. Запись невозможна" << std::endl;
-                //return 1
+        // std::ofstream file(NameAges.txt);
+        // if (!file.is_open()) {
+        // std::cout << "Закройте файл. Запись невозможна" << std::endl;
+        // return 1
 
-
-            //file << age, name << std::endl;
-
+        // file << age, name << std::endl;
 
         //}
-    }
+    } 
 }
